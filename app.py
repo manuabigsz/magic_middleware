e9eb5c4052b28daa8a59d3b690f3fcae37fb1b1a
Manuela Bertella Ossanes <80135269+manuabigsz@users.noreply.github.com>
2025-02-13 16:26:40 -0300

    feat: routes adjust

--- app.py
+++ app.py
@@ -3,13 +3,11 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-# Carregar o JSON com os dados
 with open('magic/magic.json', 'r') as f:
     items = json.load(f)
 
 @app.route("/getMagic/<int:tag_id>/<string:activity_id>", methods=["GET"])
 def getMagic(tag_id, activity_id):
-    # Procurar o item correspondente ao tag_id e activity_id fornecidos
     item = next((item for item in items if item["tag_id"] == tag_id and item["activity_id"] == activity_id), None)
 
     if item is None:
@@ -19,15 +17,33 @@ def getMagic(tag_id, activity_id):
 
 @app.route("/getQuizzes/", methods=["GET"])
 def getQuizzes():
-    # Retorna a lista de quizzes disponíveis (nome a ser exibido no carrossel)
-
-    return 0
+    try:
+        with open("magic/magic.json", "r", encoding="utf-8") as file:
+            data = json.load(file)
+        
+        quizzes = [
+            {"label": item["label"], "tag_id": item["tag_id"]}
+            for item in data if item.get("activity_id") == "quiz"
+        ]
+        
+        return jsonify(quizzes)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 @app.route("/getQuiz/<string:quizName>", methods=["GET"])
-def getMagic(quizName):
-    # Retorna a lista de quizzes disponíveis (nome a ser exibido no carrossel)
-
-    return 0
+def getQuiz(quizName):
+    try:
+        with open("quiz/animals.json", "r", encoding="utf-8") as file:
+            data = json.load(file)
+        
+        quiz_data = {
+            "quiz_name": quizName,
+            "questions": data
+        }
+        
+        return jsonify(quiz_data)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
